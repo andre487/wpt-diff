b63cc908677e5aad7dd59cd2b419f04a798f57ce
andre487 <andre487@yandex-team.ru>
2016-01-17 21:21:57 +0300

    Add initial WPTDiff tests

--- test/setup.js
+++ test/setup.js
@@ -1,3 +1,5 @@
 var chai = require('chai');
+var sinon = require('sinon');
 
 global.assert = chai.assert;
+global.sinon = sinon;
---
+++ test/wpt-diff.test.js
@@ -0,0 +1,39 @@
+var WPTDiff = require('../lib/wpt-diff').WPTDiff;
+
+describe('WPTDiff', function () {
+    var sandbox;
+
+    beforeEach(function () {
+        sandbox = sinon.sandbox.create();
+    });
+
+    afterEach(function () {
+        sandbox.restore();
+    });
+
+    describe('params', function () {
+        it('should provide API key from params', function () {
+            var wptDiff = new WPTDiff('A');
+
+            assert.equal(wptDiff.getApiKey(), 'A');
+        });
+
+        it('should provide host from params', function () {
+            var wptDiff = new WPTDiff('A', 'foo.ru');
+
+            assert.equal(wptDiff.getHost(), 'foo.ru');
+        });
+    });
+
+    describe('fool protection', function () {
+        it('should throw an exception when double run', function () {
+            var wptDiff = new WPTDiff();
+            sandbox.stub(wptDiff, '_executePipeline');
+
+            assert.throws(function () {
+                wptDiff.run([]);
+                wptDiff.run([]);
+            }, 'Already run');
+        });
+    });
+});
