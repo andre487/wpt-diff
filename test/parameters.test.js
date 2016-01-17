efd45c06d71657eb510c4d468d01810618d90165
andre487 <andre487@yandex-team.ru>
2016-01-17 20:44:26 +0300

    Implement CLI args parsing

--- test/parameters.test.js
+++ test/parameters.test.js
@@ -48,4 +48,54 @@ describe('parameters', function () {
             assert.includeMembers(params.urls, ['https://yandex.ru', 'https://google.ru']);
         });
     });
+
+    describe('#getFromCli', function () {
+        it('should provide apiKey', function () {
+            var params = parameters.getFromCli(['--api-key', 'A.foo']);
+
+            assert.propertyVal(params, 'apiKey', 'A.foo');
+        });
+
+        it('should provide location', function () {
+            var params = parameters.getFromCli(['--location', 'Moscow_IE9:Chrome']);
+
+            assert.propertyVal(params, 'location', 'Moscow_IE9:Chrome');
+        });
+
+        it('should provide connectivity', function () {
+            var params = parameters.getFromCli(['--connectivity', 'Cable']);
+
+            assert.propertyVal(params, 'connectivity', 'Cable');
+        });
+
+        it('should provide mobile param', function () {
+            var params = parameters.getFromCli(['--mobile']);
+
+            assert.propertyVal(params, 'mobile', true);
+        });
+
+        it('should provide mobile param absent', function () {
+            var params = parameters.getFromCli([]);
+
+            assert.propertyVal(params, 'mobile', false);
+        });
+
+        it('should provide labels', function () {
+            var params = parameters.getFromCli(['--label', 'Yandex', '--label', 'Google']);
+
+            assert.property(params, 'labels');
+
+            assert.lengthOf(params.labels, 2);
+            assert.includeMembers(params.labels, ['Google', 'Yandex']);
+        });
+
+        it('should provide urls', function () {
+            var params = parameters.getFromCli(['--url', 'https://yandex.ru', '--url', 'https://google.ru']);
+
+            assert.property(params, 'urls');
+
+            assert.lengthOf(params.urls, 2);
+            assert.includeMembers(params.urls, ['https://yandex.ru', 'https://google.ru']);
+        });
+    });
 });
